474dcda7cbdde82820569d10809c8ba820cc0bf9
Guistoff081 <guigolawliet13@gmail.com>
2021-11-21 23:34:29 -0300

    primeiro commit do app

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+
+function ContactPage() {
+  return (
+    <Box>
+      <Typography variant="h1">Entrar em contato: </Typography>
+    </Box>
+  );
+}
+
+export default ContactPage;
