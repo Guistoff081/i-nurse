1c518292d15b8e2c0fd07390a0ff70677bc34904
Guistoff081 <guigolawliet13@gmail.com>
2021-12-13 19:31:47 -0300

    ajustando configuraçoes

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+
+
+function AboutPage() {
+  return (
+    <Box>
+      <Typography variant="h1">Sobre o NAS</Typography>
+    </Box>
+  );
+}
+
+export default AboutPage;
