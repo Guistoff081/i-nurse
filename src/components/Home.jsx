c531d252f4b40fa98b897b046dddb1919d2d5f4a
Guistoff081 <guigolawliet13@gmail.com>
2021-12-23 20:27:58 -0300

    ajustando a home'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
-import { Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+	},
+}))
 
 function HomePage() {
+	const classes = useStyles()
 	return (
-		<Box>
-			<Typography variant="h5">Bem Vindo (a)!</Typography>
-		</Box>
+		<Grid container spacing={3}>
+			<Grid item xs={12}>
+				<Paper className={classes.paper}>xs=12</Paper>
+			</Grid>
+			<Grid item xs={12} sm={6}>
+				<Paper className={classes.paper}>xs=12 sm=6</Paper>
+			</Grid>
+			<Grid item xs={12} sm={6}>
+				<Paper className={classes.paper}>xs=12 sm=6</Paper>
+			</Grid>
+			<Grid item xs={6} sm={3}>
+				<Paper className={classes.paper}>xs=6 sm=3</Paper>
+			</Grid>
+			<Grid item xs={6} sm={3}>
+				<Paper className={classes.paper}>xs=6 sm=3</Paper>
+			</Grid>
+			<Grid item xs={6} sm={3}>
+				<Paper className={classes.paper}>xs=6 sm=3</Paper>
+			</Grid>
+			<Grid item xs={6} sm={3}>
+				<Paper className={classes.paper}>xs=6 sm=3</Paper>
+			</Grid>
+		</Grid>
 	)
 }
 
